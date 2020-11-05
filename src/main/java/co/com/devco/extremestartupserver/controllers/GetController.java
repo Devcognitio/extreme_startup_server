3d15dd142bfec82870f063c746ff4483c53dac0a
Carlos Acevedo <cacevedod@devco.com.co>
2020-11-05 17:09:10 -0500

    se agrega un logger para ver las preguntas

--- src/main/java/co/com/devco/extremestartupserver/controllers/GetController.java
+++ src/main/java/co/com/devco/extremestartupserver/controllers/GetController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 public class GetController {
     ResponderModel responder = new ResponderModel();
 
     @GetMapping("/responding")
     public String responding(@RequestParam(value = "q", defaultValue = "") String question) {
+        Logger.getAnonymousLogger().log(Level.INFO, "### Pregunta: " + question);
         return responder.answer(question);
     }
 }
